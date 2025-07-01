122d4dc8b66787b6cd1690b330f12e4a8f4ad429
reironex <nexreiro@gmail.com>
2025-07-01 16:03:50 +0800

    Create gemini.js

---
+++ api/gemini.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const { ask } = req.query;
+
+  const response = await fetch(
+    `https://kaiz-apis.gleeze.com/api/gemini-pro?ask=${encodeURIComponent(ask)}&uid=1&apikey=c219c951-e6d5-43a0-9b3a-07e037ef88d4`
+  );
+
+  const result = await response.json();
+
+  res.status(200).json({
+    reply: result.message || result.response || "No response.",
+  })
+    ;
+}
